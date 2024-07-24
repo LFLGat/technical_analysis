e764783e6d4fb1d573acec40fc0a27523205f246
Joshua Gatmaitan <joshgatmaitan286@gmail.com>
2024-07-24 18:54:08 -0400

    fixed bugs

--- static/chart.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var graphJSON = document.getElementById('graphJSON').value;
-    if (graphJSON) {
-        var graph = JSON.parse(graphJSON);
-        Plotly.newPlot('chart', graph.data, graph.layout);
-    }
-});
